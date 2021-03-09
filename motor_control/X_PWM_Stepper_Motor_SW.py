21e1057d365764ab6fdae63d626e9ee3056963a5
mangoJakeShin <72722653+mangoJakeShin@users.noreply.github.com>
2021-03-09 11:11:12 +0900

    X_Y mover

---
+++ motor_control/X_PWM_Stepper_Motor_SW.py
@@ -0,0 +1,81 @@
+from time import sleep, time
+import datetime 
+import RPi.GPIO as GPIO
+
+
+PUL = 27  # Stepper Drive Pulses
+DIR = 18  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+ENA = 17  # Controller Enable Bit (High to Enable / LOW to Disable).
+
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(PUL, GPIO.OUT)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+
+pulse = 200# This is the duration of the motor spinning. used for forward direction
+delay_org = 0.0005
+
+delay_H = delay_org # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
+delay_L = delay_H/2
+
+cycles = 50# This is the number of cycles to be run once program is started.
+cyclecount = 0 # This is the iteration of cycles to be run once program is started.
+
+def forward():
+    GPIO.output(DIR, GPIO.LOW)
+    GPIO.output(ENA, GPIO.HIGH)
+    
+    for x in range(pulse):
+        GPIO.output(PUL, GPIO.HIGH)
+        sleep(delay_H)
+        GPIO.output(PUL, GPIO.LOW)
+        sleep(delay_L)
+    return
+
+
+def reverse():
+    GPIO.output(ENA, GPIO.HIGH)
+    GPIO.output(DIR, GPIO.HIGH)
+    for y in range(pulse):
+        GPIO.output(PUL, GPIO.HIGH)
+        sleep(delay_H)
+        GPIO.output(PUL, GPIO.LOW)
+        sleep(delay_L)
+        
+    return
+
+start = time()
+timetaken = time()-start
+backnforth = 0
+# while(backnforth < 3):
+#     while cyclecount < cycles:
+#  
+#         reverse()
+#                 
+#         cyclecount = cyclecount+1
+#         print("backward {}".format(cyclecount))
+#          
+#     sleep(2)
+#     while cyclecount > 0 :
+#         forward()
+#          
+#         
+#         cyclecount = (cyclecount -1)
+#         print("forward{}".format(cyclecount))
+#     
+#     sleep(2)
+#     backnforth += 1
+# print(datetime.datetime.fromtimestamp(timetaken))
+# print(cyclecount)
+# 
+while cyclecount <3:
+    #forward()
+    reverse()
+    cyclecount += 1
+    
+GPIO.cleanup()
+print('Cycling Completed')
+#
